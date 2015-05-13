2e7c5caf5572f16381f82565e033f53bdbfb33cf
Hardik Gandhi <gandhihardikm@gmail.com>
2015-05-13 12:58:43 -0700

    removed file

--- app.js
+++ app.js
@@ -135,7 +135,17 @@ var monitoringPhrase;
 //    monitoringPhrase = "";
 //});
 
+var tweeter = new twit({
 
+    consumer_key: 'fQe0ZFl9PlW1u95gwSgHN19VM',
+
+    consumer_secret: 'GIwCckYMXbzVppNFftdFToTxLqyXjgQ79ZaZg1VzxutbWwZaVd',
+
+    access_token: 543223113+'-aH6Ja20Sz3HopfQdouUmCuPthfaz4a2leLD8AkzS',
+
+    access_token_secret: 'eKgpcveRHNnK4lKUqiDb2qBUEjKyQPjX6e22ThOJFVjnh'
+
+});
 
 //app.get('/monitor',function beginMonitoring(phrase) {
 //    var stream;
