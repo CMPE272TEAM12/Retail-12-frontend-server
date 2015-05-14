6b3a270c059757f2da03e14301f0947d37a4c2a9
Hardik Gandhi <gandhihardikm@gmail.com>
2015-05-13 17:56:33 -0700

    login

--- app.js
+++ app.js
@@ -213,7 +213,12 @@ app.get('/',
         }
     });
 
-
+var tweeter = new twit({
+    consumer_key: 'fQe0ZFl9PlW1u95gwSgHN19VM',
+    consumer_secret: 'GIwCckYMXbzVppNFftdFToTxLqyXjgQ79ZaZg1VzxutbWwZaVd',
+    access_token: 543223113+'-aH6Ja20Sz3HopfQdouUmCuPthfaz4a2leLD8AkzS',
+    access_token_secret: 'eKgpcveRHNnK4lKUqiDb2qBUEjKyQPjX6e22ThOJFVjnh'
+});
 
 
 var itemArray = new Array();
